d7bb4d262c010488dc0362a592da625ae7784dea
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-05-02 05:07:20 +0000

    Automatically backed up by Learn

--- mine.rb
+++ mine.rb
@@ -1,10 +1,15 @@
 
 
+<<<<<<< HEAD
 array=[11,9,5,8,6]
+=======
+array=[7,3,5,8,6]
+>>>>>>> 0969c51c4852aeab75f5b32bfea336bc5053064a
 count=0
 first_element=0
 
 while count<array.size do
+<<<<<<< HEAD
   if array[first_element] > array[count]
     else
       first_element=count
@@ -12,3 +17,12 @@ while count<array.size do
   count +=1
 end
 puts array[first_element]
+=======
+  if array[first_element] > array[count+1]
+    puts "yes"
+    count +=1
+  else
+    puts "no"
+  end
+end
+>>>>>>> 0969c51c4852aeab75f5b32bfea336bc5053064a
